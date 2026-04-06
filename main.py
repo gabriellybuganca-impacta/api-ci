6b857cb0097b724728bc9c5fc79d0f78fc2c414b
Gabrielly Campos Buganca <2500214@SALAS.AULAS>
2026-04-06 20:42:06 -0300

    bug proposital

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"mensagem": "API rodando!"}
-
-@app.get("/somar/{a}/{b}")
-def somar(a: int, b: int):
-    return {"resultado": a + b}
+return {"resultado": a - b}
